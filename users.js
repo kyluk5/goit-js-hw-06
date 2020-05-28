486ea71ce9bd4c0a3c19d4470c1d3ea6ca160149
Andrii Kulyk <kyluk5@gmail.com>
2020-05-28 21:37:57 +0300

    nineth_done

--- users.js
+++ users.js
@@ -188,4 +188,31 @@ const users = [
 // console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 // console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-//  
+// ==================================
+// task_9
+// Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends)
+
+const getNamesSortedByFriendsCount = users => {
+  // твой код
+  return users
+    .sort(function (prev, next) {
+      return prev.friends.length - next.friends.length
+    })
+    .map(user => user.name)
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// ===================================
+// task_10
+// Получить массив всех умений всех пользователей(поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+// const getSortedUniqueSkills = users => {
+//   // твой код
+//   return users
+
+// };
+
+// console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', '
