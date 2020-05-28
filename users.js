7419af9312ab91144787a92b05238132902514da
Andrii Kulyk <kyluk5@gmail.com>
2020-05-28 15:37:29 +0300

    sixth_done

--- users.js
+++ users.js
@@ -139,10 +139,26 @@ const users = [
 // task_5
 // Получить пользоваля (не массив) по email (поле email, он уникальный).
 
-const getUserWithEmail = (users, email) => {
+// const getUserWithEmail = (users, email) => {
+//   // твой код
+//   return users.find((user) => user.email === email);
+// };
+
+// console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+// console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+
+// ==============================
+// task_6
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => {
   // твой код
-  return users.find((user) => user.email === email);
+  return users
+    .filter((user) => user.age > min)
+    .filter((user) => user.age < max)
 };
 
-console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+// console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
